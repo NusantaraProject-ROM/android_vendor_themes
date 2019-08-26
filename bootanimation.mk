4dc25b9d9c90089fed893ffebb382672665b704c
Rizky Benggolo <travarilo@gmail.com>
2019-08-26 10:04:26 +0700

    Set bootanimation resolution for X00T
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- bootanimation.mk
+++ bootanimation.mk
@@ -14,7 +14,7 @@ ifneq ($(filter marlin,$(du_device)),)
 scr_resolution := 1440x2560
 endif
 
-ifneq ($(filter bonito sargo,$(du_device)),)
+ifneq ($(filter bonito sargo X00T,$(du_device)),)
 scr_resolution := 1080x2160
 endif
 
