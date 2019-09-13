5093281fbd3a9960d25a645734473dd7689a035b
jhenrique09 <jhenrique09.mcz@hotmail.com>
2019-09-13 16:22:36 -0300

    Add support for dark & light boot animations
    The dark theme state is stored on misc partition, that follow Pixel behavior.
    
    As reference:
    
    https://android.googlesource.com/device/google/crosshatch/+/refs/tags/android-10.0.0_r2/init.hardware.rc#994
    
    https://android.googlesource.com/platform/bootable/recovery/+/35e0f6d290ea7c12164bcfaf1b857d965e943938
    
    Change-Id: Ia2d9d163f84638d0c61b3d2e7161301aad7fc774

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,4 +1,15 @@
 # Boot Animation
+
+# Black & White
+TARGET_MISC_BLOCK_OFFSET ?= 0
+
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.misc.block.offset=$(TARGET_MISC_BLOCK_OFFSET)
+
+PRODUCT_PACKAGES += \
+    boot_theme \
+    misc_writer_system
+
 scr_resolution := 1080
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
 
