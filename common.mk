08f5befcb71fc9d0315a6862c20ed82e9bcc64d8
mydongistiny <jaysonedson@gmail.com>
2019-09-14 14:19:22 -0700

    vendor: Add Google fonts from Pixel product
    Change-Id: I2d0b810f9ae0e7552ac5b95202ae7886c8d1940f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- common.mk
+++ common.mk
@@ -5,6 +5,9 @@ DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 # Boot animation
 include vendor/themes/bootanimation.mk
 
+# Fonts
+include vendor/themes/fonts.mk
+
 # Set Pixel blue light theme on Gboard
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.ime.theme_id=5
