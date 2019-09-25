48e8577d53d5168569022e4811a00e21c5a25211
Alex Cruz <du.alexcruz@gmail.com>
2019-09-26 00:57:14 +0200

    Welcome in Android 10
    - Move inactive overlays to folder named 'inactive' until needed. Figured this
    was easier than removing them and adding them back. When needed, a simple move
    is all that's required.
    
    - Commented out unneeded fwb overlays
    - Commented out unused theme overlays and sounds

--- common.mk
+++ common.mk
@@ -2,13 +2,13 @@
 PRODUCT_PACKAGE_OVERLAYS += vendor/themes/overlay/common/
 
 # Accents
-include vendor/themes/accents.mk
+#include vendor/themes/accents.mk
 
 # Boot animation
 include vendor/themes/bootanimation.mk
 
 # Sounds
-include vendor/themes/sounds.mk
+#include vendor/themes/sounds.mk
 
 # Themes
-include vendor/themes/themes.mk
+#include vendor/themes/themes.mk
