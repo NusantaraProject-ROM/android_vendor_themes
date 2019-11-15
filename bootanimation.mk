427415f1c9eed771d805010facfe4b343ff399d9
ZVNexus <zvnexus@outlook.com>
2019-11-14 19:18:57 -0500

    New boot animation for DU 14
    Change-Id: I4130058691f860da961db38330e4ddf246f83e01
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,22 +1,6 @@
 # Boot Animation
-scr_resolution := 1080x1920
+scr_resolution := 1080
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
-
-ifneq ($(filter ,$(du_device)),)
-scr_resolution := 1440x2880
-endif
-
-ifneq ($(filter ,$(du_device)),)
-scr_resolution := 1440x2960
-endif
-
-ifneq ($(filter ,$(du_device)),)
-scr_resolution := 1440x2560
-endif
-
-ifneq ($(filter X00T lavender,$(du_device)),)
-scr_resolution := 1080x2160
-endif
 
 ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
