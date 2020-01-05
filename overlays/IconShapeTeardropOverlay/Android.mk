e50ab10734901c462b837548d62c71ccda5ad79c
spezi77 <spezi7713@gmx.net>
2020-01-05 14:25:52 +0100

    Move icon shapes to vendor [2/2].
    Signed-off-by: spezi77 <spezi7713@gmx.net>
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- overlays/IconShapeTeardropOverlay/Android.mk
+++ overlays/IconShapeTeardropOverlay/Android.mk
@@ -1,27 +1,31 @@
-# Copyright 2016-2017 The Pure Nexus Project
+#
+#  Copyright 2018, The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Overlays
-PRODUCT_PACKAGES += \
-    NexusLauncherReleaseOverlay \
-    PixelSetupWizardOverlay \
-    PixelSetupWizardAodOverlay \
-    NoCutoutOverlay
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
 
-# Icon shape overlays
-PRODUCT_PACKAGES += \
-    IconShapeRoundedRectOverlay \
-    IconShapeSquareOverlay \
-    IconShapeSquircleOverlay \
-    IconShapeTeardropOverlay
+LOCAL_RRO_THEME := IconShapeTeardrop
+
+LOCAL_PRODUCT_MODULE := true
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := IconShapeTeardropOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
