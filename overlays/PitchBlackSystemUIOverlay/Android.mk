3167b53ad822dace863330f7ccd294698d5c1a47
Alex <du.alexcruz@gmail.com>
2020-01-08 16:53:44 -0500

    Themes: Add moar themes
    Change-Id: I06ee349550f543ab7257327f9536d123d9e53c0c
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- overlays/PitchBlackSystemUIOverlay/Android.mk
+++ overlays/PitchBlackSystemUIOverlay/Android.mk
@@ -1,29 +1,31 @@
-# Copyright 2016-2017 The Pure Nexus Project
+#
+#  Copyright 2019, The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+#     http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Icon shape overlays
-PRODUCT_PACKAGES += \
-    IconShapeRoundedRectOverlay \
-    IconShapeSquareOverlay \
-    IconShapeSquircleOverlay \
-    IconShapeTeardropOverlay \
-    IconShapeCylinderOverlay \
-    IconShapeHexagonOverlay
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
 
-# Themes
-PRODUCT_PACKAGES += \
-    PitchBlackSystemOverlay \
-    PitchBlackSystemUIOverlay \
-    SolarizedDarkSystemOverlay \
-    SolarizedDarkSystemUIOverlay
+LOCAL_RRO_THEME := PitchBlackSystemUI
+
+LOCAL_PRODUCT_MODULE := true
+
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+
+LOCAL_PACKAGE_NAME := PitchBlackSystemUIOverlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
