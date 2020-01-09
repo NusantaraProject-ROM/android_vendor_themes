7e4322a50832a83fe8e598aff11b9c0df2b6f48b
Genkzsz11 <Genkzsz11@gmail.com>
2020-01-09 17:30:40 +0700

    overlays: move pixel stuff overlays in vendor du

--- overlays.mk
+++ overlays.mk
@@ -12,13 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Overlays
-PRODUCT_PACKAGES += \
-    NexusLauncherReleaseOverlay \
-    PixelSetupWizardOverlay \
-    PixelSetupWizardAodOverlay \
-    NoCutoutOverlay
-
 # Icon shape overlays
 PRODUCT_PACKAGES += \
     IconShapeRoundedRectOverlay \
