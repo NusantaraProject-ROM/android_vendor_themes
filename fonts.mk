a22dc402d3ec228b9fb3e26e2a3501c910d15283
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-17 22:17:47 -0500

    Fonts: Add our favorite fonts into the AOSP Font Engine
    Change-Id: Ie3bd3b2ebceccdf9d10d680c28bbd3b162193105

--- fonts.mk
+++ fonts.mk
@@ -1,3 +1,14 @@
 # Fonts
-PRODUCT_PACKAGES := \
-    fonts.xml
+PRODUCT_PACKAGES += \
+    fonts.xml \
+    FontCagliostroSourceOverlay \
+    FontLGSmartGothicSourceOverlay \
+    FontRosemarySourceOverlay \
+    FontSonySketchSourceOverlay
+
+
+PRODUCT_COPY_FILES += \
+    vendor/themes/fonts/prebuilt/Cagliostro-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Cagliostro-Regular.ttf \
+    vendor/themes/fonts/prebuilt/LGSmartGothic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/LGSmartGothic.ttf \
+    vendor/themes/fonts/prebuilt/Rosemary-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Rosemary-Regular.ttf \
+    vendor/themes/fonts/prebuilt/SonySketch.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SonySketch.ttf
