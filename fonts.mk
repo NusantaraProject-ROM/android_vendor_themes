acaf4f034ce606fc66ddfa43fddfa4b5c102943f
Genkzsz11 <Genkzsz11@gmail.com>
2020-01-20 09:24:43 +0700

    Add arbutus font
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- fonts.mk
+++ fonts.mk
@@ -1,13 +1,7 @@
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/themes/fonts,$(TARGET_COPY_OUT_PRODUCT)/fonts)
 
 PRODUCT_COPY_FILES += \
-    vendor/themes/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
-    vendor/themes/fonts/GoogleSans/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/themes/fonts/GoogleSans/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Bold.ttf \
-    vendor/themes/fonts/GoogleSans/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic.ttf \
-    vendor/themes/fonts/GoogleSans/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/themes/fonts/GoogleSans/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Medium.ttf \
-    vendor/themes/fonts/GoogleSans/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular.ttf
+    vendor/themes/etc/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml
 
 # Favourite fonts
 PRODUCT_COPY_FILES += \
