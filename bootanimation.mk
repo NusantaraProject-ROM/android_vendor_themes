b1d1481986473ee779110c6e99b613804364f501
Genkzsz11 <Genkzsz11@gmail.com>
2020-02-07 23:14:01 +0700

    bootanimation: 1440 for my device
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

--- bootanimation.mk
+++ bootanimation.mk
@@ -13,6 +13,10 @@ PRODUCT_PACKAGES += \
 scr_resolution := 1080
 du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
 
+ifneq ($(filter lavender,$(du_device)),)
+scr_resolution := 1440
+endif
+
 ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
 PRODUCT_COPY_FILES += \
     vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip \
