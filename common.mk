fcba5e749b73b52a86750702ff15a5c93d810034
Hikari-no-Tenshi <kyryljan.serhij@gmail.com>
2020-03-12 14:16:31 +0200

    FODAnimation: Make animation resources optional [3/3]

--- common.mk
+++ common.mk
@@ -2,6 +2,11 @@
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 
+ifeq ($(EXTRA_FOD_ANIMATIONS),true)
+DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/fod
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/fod
+endif
+
 # Boot animation
 include vendor/themes/bootanimation.mk
 
