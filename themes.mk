494020c0cfbca6ec5591f4bee29a7daf684b0e33
SagarMakhar <sagarmakhar@gmail.com>
2020-03-13 10:27:17 +0000

    themes: Add more icon shape overlays
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- themes.mk
+++ themes.mk
@@ -55,14 +55,18 @@ PRODUCT_PACKAGES += \
 
 # Icon Shapes
 PRODUCT_PACKAGES += \
+    IconShapeCloudyOverlay \
     IconShapeCylinderOverlay \
+    IconShapeFlowerOverlay \
     IconShapeHeartOverlay \
     IconShapeHexagonOverlay \
     IconShapeLeafOverlay \
     IconShapePebbleOverlay \
+    IconShapeRoundedHexagonOverlay \
     IconShapeRoundedRectOverlay \
     IconShapeSquareOverlay \
     IconShapeSquircleOverlay \
+    IconShapeStretchedOverlay \
     IconShapeTaperedRectOverlay \
     IconShapeTeardropOverlay \
     IconShapeVesselOverlay
