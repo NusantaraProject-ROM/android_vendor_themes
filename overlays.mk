9d238d3e057e4ddcdfb0d03dc6c44016df46016c
Genkzsz11 <Genkzsz11@gmail.com>
2020-03-29 22:59:27 +0700

    Clean up Notch [3/3]
    This reverts commit d39eeccffbc88d722985b29206b02bde3c8d64ca.

--- overlays.mk
+++ overlays.mk
@@ -12,10 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Cutout
-PRODUCT_PACKAGES += \
-    NoCutoutOverlay
-
 # Icon shape overlays
 PRODUCT_PACKAGES += \
     IconShapeRoundedRectOverlay \
