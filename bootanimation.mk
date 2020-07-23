fd47d3bada7a6b984e7849b841e929f34f293428
Genkzsz11 <Genkzsz11@gmail.com>
2020-07-24 06:03:13 +0700

    bootanimation: add newflag
    Change-Id: Ic9e9dc7d25f4b9273b0ebbd25e167b63529ab11a

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,16 +1,7 @@
 # Boot Animation
 
-du_device := $(patsubst %f,%,$(subst du_,,$(TARGET_PRODUCT)))
-
-ifneq ($(filter raphael enchilada lavender mido wayne whyred platina ginkgo vince twolip beryllium violet X00T RMX1801 tissot x2 jasmine_sprout sirius grus,$(du_device)),)
-scr_resolution := 1080
-endif
-
-ifneq ($(filter ysl riva land santoni X00P kenzo,$(du_device)),)
-scr_resolution := 720
-endif
-
-ifneq ($(wildcard vendor/themes/bootanimation/$(scr_resolution).zip),)
-PRODUCT_COPY_FILES += \
-    vendor/themes/bootanimation/$(scr_resolution).zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+PRODUCT_COPY_FILES += vendor/themes/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
+PRODUCT_COPY_FILES += vendor/themes/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 endif
