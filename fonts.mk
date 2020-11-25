61b9b52eed4b50cb65ca037ea7b3ca8237378447
Genkzsz11 <genkzsz11@nusantaradevs.org>
2020-11-26 05:52:16 +0700

    Checkout some prebuilt font from xtended
    Signed-off-by: Genkzsz11 <genkzsz11@nusantaradevs.org>

--- fonts.mk
+++ fonts.mk
@@ -14,41 +14,4 @@ PRODUCT_PACKAGES += \
     FontCaviarDreamsSourceOverlay \
     FontLinotteOverlay
 
-PRODUCT_COPY_FILES += \
-    vendor/themes/fonts/prebuilt/Cagliostro-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Cagliostro-Regular.ttf \
-    vendor/themes/fonts/prebuilt/LGSmartGothic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/LGSmartGothic.ttf \
-    vendor/themes/fonts/prebuilt/Rosemary-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Rosemary-Regular.ttf \
-    vendor/themes/fonts/prebuilt/SonySketch.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SonySketch.ttf \
-    vendor/themes/fonts/prebuilt/GoogleSans-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Bold.ttf \
-    vendor/themes/fonts/prebuilt/GoogleSans-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-BoldItalic.ttf \
-    vendor/themes/fonts/prebuilt/GoogleSans-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Italic.ttf \
-    vendor/themes/fonts/prebuilt/GoogleSans-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Medium.ttf \
-    vendor/themes/fonts/prebuilt/GoogleSans-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-MediumItalic.ttf \
-    vendor/themes/fonts/prebuilt/GoogleSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/GoogleSans-Regular.ttf \
-    vendor/themes/fonts/prebuilt/Linotte.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Linotte.ttf \
-    vendor/themes/fonts/prebuilt/SamsungOne.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SamsungOne.ttf \
-    vendor/themes/fonts/prebuilt/SlateFromOP-Light.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SlateFromOP-Light.ttf \
-    vendor/themes/fonts/prebuilt/SlateFromOP-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/SlateFromOP-Regular.ttf \
-    vendor/themes/fonts/prebuilt/FiraSans-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Bold.ttf \
-    vendor/themes/fonts/prebuilt/FiraSans-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-BoldItalic.ttf \
-    vendor/themes/fonts/prebuilt/FiraSans-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Italic.ttf \
-    vendor/themes/fonts/prebuilt/FiraSans-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Medium.ttf \
-    vendor/themes/fonts/prebuilt/FiraSans-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-MediumItalic.ttf \
-    vendor/themes/fonts/prebuilt/FiraSans-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/FiraSans-Regular.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-Bold.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-BoldItalic.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-Italic.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-Light.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-Light.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-LightItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-LightItalic.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-Regular.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-SemiBold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-SemiBold.ttf \
-    vendor/themes/fonts/prebuilt/TitilliumWeb-SemiBoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/TitilliumWeb-SemiBoldItalic.ttf \
-    vendor/themes/fonts/prebuilt/Ubuntu-Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Ubuntu-Bold.ttf \
-    vendor/themes/fonts/prebuilt/Ubuntu-BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Ubuntu-BoldItalic.ttf \
-    vendor/themes/fonts/prebuilt/Ubuntu-Medium.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Ubuntu-Medium.ttf \
-    vendor/themes/fonts/prebuilt/Ubuntu-MediumItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Ubuntu-MediumItalic.ttf \
-    vendor/themes/fonts/prebuilt/Ubuntu-Regular.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/Ubuntu-Regular.ttf \
-    vendor/themes/fonts/prebuilt/CaviarDreams.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/CaviarDreams.ttf \
-    vendor/themes/fonts/prebuilt/CaviarDreams_Bold.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/CaviarDreams_Bold.ttf \
-    vendor/themes/fonts/prebuilt/CaviarDreams_BoldItalic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/CaviarDreams_BoldItalic.ttf \
-    vendor/themes/fonts/prebuilt/CaviarDreams_Italic.ttf:$(TARGET_COPY_OUT_SYSTEM)/fonts/CaviarDreams_Italic.ttf
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/themes/fonts/prebuilt,$(TARGET_COPY_OUT_SYSTEM)/fonts)
