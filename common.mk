3e0658a80484346321fa74a9c4884282e70a5c9c
DennySPb <dennyspb@gmail.com>
2020-11-30 15:08:52 +0300

    Move FOD animation resources to standalone package [2/2]
    Change-Id: I4d1e483da50623c5d47dc5d831fa128b8b8e7ca4
    Signed-off-by: DennySPb <dennyspb@gmail.com>

--- common.mk
+++ common.mk
@@ -18,6 +18,12 @@ include vendor/themes/fonts.mk
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.ime.theme_id=5
 
+# FOD
+ifeq ($(EXTRA_FOD_ANIMATIONS),true)
+PRODUCT_PACKAGES += \
+    FodAnimationResources
+endif
+
 # Sounds
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=The_big_adventure.ogg \
