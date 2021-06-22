1e4e94075140e28c2416337fd09e0698a1fc9a9a
Genkzsz11 <Genkzsz11@gmail.com>
2021-06-22 17:11:40 +0700

    font: Remove fonts [3/3]

--- fonts.mk
+++ fonts.mk
@@ -1,17 +1,5 @@
 # Fonts
 PRODUCT_PACKAGES += \
-    fonts.xml \
-    FontCagliostroSourceOverlay \
-    FontLGSmartGothicSourceOverlay \
-    FontRosemarySourceOverlay \
-    FontSonySketchSourceOverlay \
-    FontGoogleSansSourceOverlay \
-    FontOneplusSlateSource \
-    FontSamsungOneSourceOverlay \
-    FontFiraSansSourceOverlay \
-    FontUbuntuSourceOverlay \
-    FontTitilliumWebSourceOverlay \
-    FontCaviarDreamsSourceOverlay \
-    FontLinotteOverlay
+    fonts.xml
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/themes/fonts/prebuilt,$(TARGET_COPY_OUT_SYSTEM)/fonts)
