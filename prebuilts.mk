5127d680d2915cbed089a59d7ab02073d9cc03f1
Mocarafee <harif.rp05@gmail.com>
2021-08-16 11:09:17 +0700

    prebuilt: Add conditional to build prebuilt apps
    Signed-off-by: Mocarafee <harif.rp05@gmail.com>

--- prebuilts.mk
+++ prebuilts.mk
@@ -1,3 +1,13 @@
 # Prebuilt app
 PRODUCT_PACKAGES += \
    SoundPickerPrebuilt
+
+ifneq ($(USE_GAPPS), true)
+PRODUCT_PACKAGES += \
+   GalleryGoPrebuilt \
+   ViaPrebuilt
+ifeq ($(USE_MICROG),true)
+PRODUCT_PACKAGES += \
+   SimpleGalleryPrebuilt
+endif
+endif
