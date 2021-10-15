ProtonAOSP/android_vendor_proton

fonts: Include extra fonts in generated fonts.mk

fonts/generate_build.sh
@@ -68,3 +68,7 @@ prebuilt_font {
     echo "    $android_name \\" >> fonts.mk
 
 done
+
+echo '
+# Include extra addon fonts
+-include vendor/proton/fonts/extra/extra.mk' >> fonts.mk

NusantaraProject-ROM/android_vendor_themes

Signed-off-by: Dmitrii <bankersenator@gmail.com>

fonts/generate_build.sh
@@ -68,3 +68,7 @@ prebuilt_font {
     echo "    $android_name \\" >> fonts.mk
 
 done
+
+echo '
+# Include extra addon fonts
+-include vendor/themes/fonts/extra/extra.mk' >> fonts.mk