ProtonAOSP/android_vendor_proton

fonts: Add font styles for ThemePicker
Many users have requested font styles other than the default (Inter), so
let's add some.

All of the following fonts are licensed under the Open Font License.
Variable fonts were used whenever possible to reduce file size.

- Fira Sans
- HK Grotesk
- Manrope
- Noto Sans
- Recursive
- Roboto
- Rubik
- Source Sans

fonts/font_files.mk
@@ -1,0 +1,80 @@
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/proton/fonts
+
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES += \
+    CutiveMono.ttf \
+    DroidSansMono.ttf \
+    Manrope-VF.ttf \
+    NotoSans-Italic-VF.ttf \
+    NotoSans-VF.ttf \
+    Recursive-VF.ttf \
+    RobotoFallback-VF.ttf \
+    RobotoSystem-VF.ttf \
+    Rubik-Italic-VF.ttf \
+    Rubik-VF.ttf \
+    FiraSans-BlackItalic.otf \
+    FiraSans-Black.otf \
+    FiraSans-BoldItalic.otf \
+    FiraSans-Bold.otf \
+    FiraSans-Italic.otf \
+    FiraSans-LightItalic.otf \
+    FiraSans-Light.otf \
+    FiraSans-MediumItalic.otf \
+    FiraSans-Medium.otf \
+    FiraSans-Regular.otf \
+    FiraSans-ThinItalic.otf \
+    FiraSans-Thin.otf \
+    HKGrotesk-BlackItalic.otf \
+    HKGrotesk-Black.otf \
+    HKGrotesk-BoldItalic.otf \
+    HKGrotesk-Bold.otf \
+    HKGrotesk-Italic.otf \
+    HKGrotesk-LightItalic.otf \
+    HKGrotesk-Light.otf \
+    HKGrotesk-MediumItalic.otf \
+    HKGrotesk-Medium.otf \
+    HKGrotesk-Regular.otf \
+    HKGrotesk-ThinItalic.otf \
+    HKGrotesk-Thin.otf \
+    Roboto-BlackItalic.ttf \
+    Roboto-Black.ttf \
+    Roboto-BoldItalic.ttf \
+    Roboto-Bold.ttf \
+    Roboto-Italic.ttf \
+    Roboto-LightItalic.ttf \
+    Roboto-Light.ttf \
+    Roboto-MediumItalic.ttf \
+    Roboto-Medium.ttf \
+    Roboto-Regular.ttf \
+    Roboto-ThinItalic.ttf \
+    Roboto-Thin.ttf \
+    SourceSans3-Italic-VF.otf \
+    SourceSans3-VF.otf \
+    NotoSerif-BoldItalic.ttf \
+    NotoSerif-Bold.ttf \
+    NotoSerif-Italic.ttf \
+    NotoSerif-Regular.ttf \
+
+# Register new fonts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+
+# Include extra addon fonts
+-include vendor/proton/fonts/extra/extra.mk

fonts/fonts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 The Proton AOSP Project
+# Copyright (C) 2021 The Proton AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,41 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
+LOCAL_PATH := vendor/proton/fonts
 
-PRODUCT_PACKAGES += \
-    CutiveMono.ttf \
-    DroidSansMono.ttf \
-    RobotoFallback-BlackItalic.ttf \
-    RobotoFallback-Black.ttf \
-    RobotoFallback-BoldItalic.ttf \
-    RobotoFallback-Bold.ttf \
-    RobotoFallback-Italic.ttf \
-    RobotoFallback-LightItalic.ttf \
-    RobotoFallback-Light.ttf \
-    RobotoFallback-MediumItalic.ttf \
-    RobotoFallback-Medium.ttf \
-    RobotoFallback-Regular.ttf \
-    RobotoFallback-ThinItalic.ttf \
-    RobotoFallback-Thin.ttf \
-    Roboto-BlackItalic.ttf \
-    Roboto-Black.ttf \
-    Roboto-BoldItalic.ttf \
-    Roboto-Bold.ttf \
-    Roboto-Italic.ttf \
-    Roboto-LightItalic.ttf \
-    Roboto-Light.ttf \
-    Roboto-MediumItalic.ttf \
-    Roboto-Medium.ttf \
-    Roboto-Regular.ttf \
-    Roboto-ThinItalic.ttf \
-    Roboto-Thin.ttf \
-    NotoSerif-BoldItalic.ttf \
-    NotoSerif-Bold.ttf \
-    NotoSerif-Italic.ttf \
-    NotoSerif-Regular.ttf \
-
-# Include extra addon fonts
+# Copy font files
+include vendor/proton/fonts/font_files.mk
 -include vendor/proton/fonts/extra/extra.mk
+
+# Register custom fonts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+
+# Overlays for UI font styles
+PRODUCT_PACKAGES += \
+    FontRobotoOverlay \
+    FontFiraSansOverlay \
+    FontHKGroteskOverlay \
+    FontManropeOverlay \
+    FontNotoSansOverlay \
+    FontRecursiveCasualOverlay \
+    FontRecursiveLinearOverlay \
+    FontRubikOverlay \
+    FontSourceSansOverlay \
+    FontSourceSerifOverlay \

fonts/generate_build.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 
 q='"'
 
-echo '# Copyright (C) 2020 The Proton AOSP Project
+echo '# Copyright (C) 2021 The Proton AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,12 +20,14 @@ echo '# Copyright (C) 2020 The Proton AOSP Project
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-' > fonts.mk
-echo '# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+
+LOCAL_PATH := vendor/proton/fonts
+
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
 # PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
 # get installed too.
 
-PRODUCT_PACKAGES += \' >> fonts.mk
+PRODUCT_PACKAGES += \' > font_files.mk
 
 rm -f */Android.bp
 
@@ -36,12 +38,22 @@ do
     font_dir="$(dirname "$src")"
     src_base="$(basename "$(basename "$src" .ttf)" .otf)"
     src_name="$(basename "$src")"
-    if [[ "$src" == *"_"* ]]; then
-    	# If a name was explicitly specified with an underscore, use it
-	android_name="$(cut -d'_' -f2 <<< "$src_base").ttf"
+    src_ext="${src##*.}"
+
+    if [[ -f "$font_dir/product.flag" ]]; then
+        product_flag="
+    product_specific: true,"
     else
-    	# Use heuristics to construct a name
-	android_name="$(sed -e 's/Inter/Roboto/; s/SFUIText/Roboto/; s/SourceSerif4/NotoSerif/' <<< "$src_base").ttf"
+        product_flag=""
+        src_ext="ttf"
+    fi
+
+    if [[ "$src" == *"_"* ]]; then
+        # If a name was explicitly specified with an underscore, use it
+        android_name="$(cut -d'_' -f2 <<< "$src_base").$src_ext"
+    else
+        # Use heuristics to construct a name
+        android_name="$(sed -e 's/Inter/Roboto/; s/SFUIText/Roboto/; s/SourceSerif4/NotoSerif/' <<< "$src_base").$src_ext"
     fi
 
     if [[ ! -f "$font_dir/Android.bp" ]]; then
@@ -65,12 +77,16 @@ do
     echo "
 prebuilt_font {
     name: ${q}$android_name${q},
-    src: ${q}$src_name${q},
+    src: ${q}$src_name${q},$product_flag
 }" >> "$font_dir/Android.bp"
-    echo "    $android_name \\" >> fonts.mk
+    echo "    $android_name \\" >> font_files.mk
 
 done
 
 echo '
+# Register new fonts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+
 # Include extra addon fonts
--include vendor/proton/fonts/extra/extra.mk' >> fonts.mk
+-include vendor/proton/fonts/extra/extra.mk' >> font_files.mk

fonts/generate_overlays.py
@@ -1,0 +1,129 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+
+DEST_DIR = f"{os.path.dirname(os.path.realpath(__file__))}/overlays"
+
+# (user-facing name, font family name)
+FONTS = {
+    "Fira Sans": "fira-sans",
+    "HK Grotesk": "hk-grotesk",
+    "Manrope": "manrope",
+    "Noto Sans": "noto-sans",
+    "Recursive Casual": "recursive-casual",
+    "Recursive Linear": "recursive",
+    "Roboto": "roboto-system",
+    "Rubik": "rubik",
+    "Source Sans": "source-sans",
+    "Source Serif": "serif",
+}
+
+# Android.mk
+ANDROID_MK_TEMPLATE = """#
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := Font%APKNAME%
+LOCAL_PRODUCT_MODULE := true
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := Font%APKNAME%Overlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
+"""
+
+# AndroidManifest.xml
+ANDROID_MANIFEST_TEMPLATE = """<!--
+    Copyright (C) 2021 The Proton AOSP Project
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+-->
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="org.protonaosp.theme.font.%PKGNAME%">
+
+    <overlay android:targetPackage="android"
+        android:category="android.theme.customization.font"
+        android:priority="1" />
+
+    <application
+        android:label="%USERNAME%"
+        android:hasCode="false" />
+
+</manifest>
+"""
+
+# config.xml
+CONFIG_XML_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<!--
+    Copyright (C) 2021 The Proton AOSP Project
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+-->
+<resources>
+    <string name="config_bodyFontFamily" translatable="false">%FONTNAME%</string>
+    <string name="config_bodyFontFamilyMedium" translatable="false">%FONTNAME%-medium</string>
+    <string name="config_headlineFontFamily" translatable="false">%FONTNAME%</string>
+    <string name="config_headlineFontFamilyMedium" translatable="false">%FONTNAME%-medium</string>
+</resources>
+"""
+
+def main():
+    for user_name, family_name in FONTS.items():
+        pkg_apk_name = user_name.replace(" ", "")
+        pkg_name = user_name.lower().replace(" ", "_")
+        apk_name = f"Font{pkg_apk_name}Overlay"
+        pkg_dir = f"{DEST_DIR}/{apk_name}"
+
+        print(apk_name)
+        shutil.rmtree(pkg_dir, ignore_errors=True)
+
+        xml_dir = f"{pkg_dir}/res/values"
+        os.makedirs(xml_dir, exist_ok=True)
+        with open(f"{xml_dir}/config.xml", "w+") as f:
+            f.write(CONFIG_XML_TEMPLATE.replace("%FONTNAME%", family_name))
+
+        with open(f"{pkg_dir}/Android.mk", "w+") as f:
+            f.write(ANDROID_MK_TEMPLATE.replace("%APKNAME%", pkg_apk_name))
+
+        with open(f"{pkg_dir}/AndroidManifest.xml", "w+") as f:
+            f.write(ANDROID_MANIFEST_TEMPLATE.replace("%PKGNAME%", pkg_name).replace("%USERNAME%", user_name))
+
+if __name__ == '__main__':
+    main()

NusantaraProject-ROM/android_vendor_themes

Signed-off-by: Dmitrii <bankersenator@gmail.com>
Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>

fonts/font_files.mk
@@ -1,0 +1,80 @@
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+LOCAL_PATH := vendor/themes/fonts
+
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
+# get installed too.
+
+PRODUCT_PACKAGES += \
+    CutiveMono.ttf \
+    DroidSansMono.ttf \
+    Manrope-VF.ttf \
+    NotoSans-Italic-VF.ttf \
+    NotoSans-VF.ttf \
+    Recursive-VF.ttf \
+    RobotoFallback-VF.ttf \
+    RobotoSystem-VF.ttf \
+    Rubik-Italic-VF.ttf \
+    Rubik-VF.ttf \
+    FiraSans-BlackItalic.otf \
+    FiraSans-Black.otf \
+    FiraSans-BoldItalic.otf \
+    FiraSans-Bold.otf \
+    FiraSans-Italic.otf \
+    FiraSans-LightItalic.otf \
+    FiraSans-Light.otf \
+    FiraSans-MediumItalic.otf \
+    FiraSans-Medium.otf \
+    FiraSans-Regular.otf \
+    FiraSans-ThinItalic.otf \
+    FiraSans-Thin.otf \
+    HKGrotesk-BlackItalic.otf \
+    HKGrotesk-Black.otf \
+    HKGrotesk-BoldItalic.otf \
+    HKGrotesk-Bold.otf \
+    HKGrotesk-Italic.otf \
+    HKGrotesk-LightItalic.otf \
+    HKGrotesk-Light.otf \
+    HKGrotesk-MediumItalic.otf \
+    HKGrotesk-Medium.otf \
+    HKGrotesk-Regular.otf \
+    HKGrotesk-ThinItalic.otf \
+    HKGrotesk-Thin.otf \
+    Roboto-BlackItalic.ttf \
+    Roboto-Black.ttf \
+    Roboto-BoldItalic.ttf \
+    Roboto-Bold.ttf \
+    Roboto-Italic.ttf \
+    Roboto-LightItalic.ttf \
+    Roboto-Light.ttf \
+    Roboto-MediumItalic.ttf \
+    Roboto-Medium.ttf \
+    Roboto-Regular.ttf \
+    Roboto-ThinItalic.ttf \
+    Roboto-Thin.ttf \
+    SourceSans3-Italic-VF.otf \
+    SourceSans3-VF.otf \
+    NotoSerif-BoldItalic.ttf \
+    NotoSerif-Bold.ttf \
+    NotoSerif-Italic.ttf \
+    NotoSerif-Regular.ttf \
+
+# Register new fonts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+
+# Include extra addon fonts
+-include vendor/themes/fonts/extra/extra.mk

fonts/fonts.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2020 The Proton AOSP Project
+# Copyright (C) 2021 The Proton AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,41 +12,25 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
-# PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
-# get installed too.
+LOCAL_PATH := vendor/themes/fonts
 
-PRODUCT_PACKAGES += \
-    CutiveMono.ttf \
-    DroidSansMono.ttf \
-    RobotoFallback-BlackItalic.ttf \
-    RobotoFallback-Black.ttf \
-    RobotoFallback-BoldItalic.ttf \
-    RobotoFallback-Bold.ttf \
-    RobotoFallback-Italic.ttf \
-    RobotoFallback-LightItalic.ttf \
-    RobotoFallback-Light.ttf \
-    RobotoFallback-MediumItalic.ttf \
-    RobotoFallback-Medium.ttf \
-    RobotoFallback-Regular.ttf \
-    RobotoFallback-ThinItalic.ttf \
-    RobotoFallback-Thin.ttf \
-    Roboto-BlackItalic.ttf \
-    Roboto-Black.ttf \
-    Roboto-BoldItalic.ttf \
-    Roboto-Bold.ttf \
-    Roboto-Italic.ttf \
-    Roboto-LightItalic.ttf \
-    Roboto-Light.ttf \
-    Roboto-MediumItalic.ttf \
-    Roboto-Medium.ttf \
-    Roboto-Regular.ttf \
-    Roboto-ThinItalic.ttf \
-    Roboto-Thin.ttf \
-    NotoSerif-BoldItalic.ttf \
-    NotoSerif-Bold.ttf \
-    NotoSerif-Italic.ttf \
-    NotoSerif-Regular.ttf \
-
-# Include extra addon fonts
+# Copy font files
+include vendor/themes/fonts/font_files.mk
 -include vendor/themes/fonts/extra/extra.mk
+
+# Register custom fonts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+
+# Overlays for UI font styles
+PRODUCT_PACKAGES += \
+    FontRobotoOverlay \
+    FontFiraSansOverlay \
+    FontHKGroteskOverlay \
+    FontManropeOverlay \
+    FontNotoSansOverlay \
+    FontRecursiveCasualOverlay \
+    FontRecursiveLinearOverlay \
+    FontRubikOverlay \
+    FontSourceSansOverlay \
+    FontSourceSerifOverlay \

fonts/generate_build.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 
 q='"'
 
-echo '# Copyright (C) 2020 The Proton AOSP Project
+echo '# Copyright (C) 2021 The Proton AOSP Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -20,12 +20,14 @@ echo '# Copyright (C) 2020 The Proton AOSP Project
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-' > fonts.mk
-echo '# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
+
+LOCAL_PATH := vendor/themes/fonts
+
+# We have to use PRODUCT_PACKAGES (together with BUILD_PREBUILT) instead of
 # PRODUCT_COPY_FILES to install the font files, so that the NOTICE file can
 # get installed too.
 
-PRODUCT_PACKAGES += \' >> fonts.mk
+PRODUCT_PACKAGES += \' > font_files.mk
 
 rm -f */Android.bp
 
@@ -36,12 +38,22 @@ do
     font_dir="$(dirname "$src")"
     src_base="$(basename "$(basename "$src" .ttf)" .otf)"
     src_name="$(basename "$src")"
-    if [[ "$src" == *"_"* ]]; then
-    	# If a name was explicitly specified with an underscore, use it
-	android_name="$(cut -d'_' -f2 <<< "$src_base").ttf"
+    src_ext="${src##*.}"
+
+    if [[ -f "$font_dir/product.flag" ]]; then
+        product_flag="
+    product_specific: true,"
     else
-    	# Use heuristics to construct a name
-	android_name="$(sed -e 's/Inter/Roboto/; s/SFUIText/Roboto/; s/SourceSerif4/NotoSerif/' <<< "$src_base").ttf"
+        product_flag=""
+        src_ext="ttf"
+    fi
+
+    if [[ "$src" == *"_"* ]]; then
+        # If a name was explicitly specified with an underscore, use it
+        android_name="$(cut -d'_' -f2 <<< "$src_base").$src_ext"
+    else
+        # Use heuristics to construct a name
+        android_name="$(sed -e 's/Inter/Roboto/; s/SFUIText/Roboto/; s/SourceSerif4/NotoSerif/' <<< "$src_base").$src_ext"
     fi
 
     if [[ ! -f "$font_dir/Android.bp" ]]; then
@@ -65,12 +77,16 @@ do
     echo "
 prebuilt_font {
     name: ${q}$android_name${q},
-    src: ${q}$src_name${q},
+    src: ${q}$src_name${q},$product_flag
 }" >> "$font_dir/Android.bp"
-    echo "    $android_name \\" >> fonts.mk
+    echo "    $android_name \\" >> font_files.mk
 
 done
 
 echo '
+# Register new fonts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/fonts_customization.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/fonts_customization.xml \
+
 # Include extra addon fonts
--include vendor/themes/fonts/extra/extra.mk' >> fonts.mk
+-include vendor/themes/fonts/extra/extra.mk' >> font_files.mk

fonts/generate_overlays.py
@@ -1,0 +1,129 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import shutil
+
+DEST_DIR = f"{os.path.dirname(os.path.realpath(__file__))}/overlays"
+
+# (user-facing name, font family name)
+FONTS = {
+    "Fira Sans": "fira-sans",
+    "HK Grotesk": "hk-grotesk",
+    "Manrope": "manrope",
+    "Noto Sans": "noto-sans",
+    "Recursive Casual": "recursive-casual",
+    "Recursive Linear": "recursive",
+    "Roboto": "roboto-system",
+    "Rubik": "rubik",
+    "Source Sans": "source-sans",
+    "Source Serif": "serif",
+}
+
+# Android.mk
+ANDROID_MK_TEMPLATE = """#
+# Copyright (C) 2021 The Proton AOSP Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := Font%APKNAME%
+LOCAL_PRODUCT_MODULE := true
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := Font%APKNAME%Overlay
+LOCAL_SDK_VERSION := current
+
+include $(BUILD_RRO_PACKAGE)
+"""
+
+# AndroidManifest.xml
+ANDROID_MANIFEST_TEMPLATE = """<!--
+    Copyright (C) 2021 The Proton AOSP Project
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+-->
+<manifest xmlns:android="http://schemas.android.com/apk/res/android"
+    package="com.nusantara.theme.font.%PKGNAME%">
+
+    <overlay android:targetPackage="android"
+        android:category="android.theme.customization.font"
+        android:priority="1" />
+
+    <application
+        android:label="%USERNAME%"
+        android:hasCode="false" />
+
+</manifest>
+"""
+
+# config.xml
+CONFIG_XML_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<!--
+    Copyright (C) 2021 The Proton AOSP Project
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+-->
+<resources>
+    <string name="config_bodyFontFamily" translatable="false">%FONTNAME%</string>
+    <string name="config_bodyFontFamilyMedium" translatable="false">%FONTNAME%-medium</string>
+    <string name="config_headlineFontFamily" translatable="false">%FONTNAME%</string>
+    <string name="config_headlineFontFamilyMedium" translatable="false">%FONTNAME%-medium</string>
+</resources>
+"""
+
+def main():
+    for user_name, family_name in FONTS.items():
+        pkg_apk_name = user_name.replace(" ", "")
+        pkg_name = user_name.lower().replace(" ", "_")
+        apk_name = f"Font{pkg_apk_name}Overlay"
+        pkg_dir = f"{DEST_DIR}/{apk_name}"
+
+        print(apk_name)
+        shutil.rmtree(pkg_dir, ignore_errors=True)
+
+        xml_dir = f"{pkg_dir}/res/values"
+        os.makedirs(xml_dir, exist_ok=True)
+        with open(f"{xml_dir}/config.xml", "w+") as f:
+            f.write(CONFIG_XML_TEMPLATE.replace("%FONTNAME%", family_name))
+
+        with open(f"{pkg_dir}/Android.mk", "w+") as f:
+            f.write(ANDROID_MK_TEMPLATE.replace("%APKNAME%", pkg_apk_name))
+
+        with open(f"{pkg_dir}/AndroidManifest.xml", "w+") as f:
+            f.write(ANDROID_MANIFEST_TEMPLATE.replace("%PKGNAME%", pkg_name).replace("%USERNAME%", user_name))
+
+if __name__ == '__main__':
+    main()