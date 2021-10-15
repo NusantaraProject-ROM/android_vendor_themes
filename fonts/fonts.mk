ProtonAOSP/android_vendor_proton

fonts: Include extra addon fonts if available
Some fonts cannot be in public source code repositories due to licensing
issues, so we need to allow including extra addon fonts to build with
them.

fonts/fonts.mk
@@ -21,3 +21,6 @@ PRODUCT_PACKAGES := \
     NotoSerif-Bold.ttf \
     NotoSerif-Italic.ttf \
     NotoSerif-Regular.ttf \
+
+# Include extra addon fonts
+-include vendor/proton/fonts/extra/extra.mk

NusantaraProject-ROM/android_vendor_themes

Signed-off-by: Dmitrii <bankersenator@gmail.com>

fonts/fonts.mk
@@ -21,3 +21,6 @@ PRODUCT_PACKAGES := \
     NotoSerif-Bold.ttf \
     NotoSerif-Italic.ttf \
     NotoSerif-Regular.ttf \
+
+# Include extra addon fonts
+-include vendor/themes/fonts/extra/extra.mk