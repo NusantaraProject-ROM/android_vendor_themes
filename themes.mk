ef87881d61a34cf7fbc56d7d17920b1aff2eb706
Genkzsz11 <genkzsz11@gmail.com>
2022-02-26 21:13:21 +0700

    navbar: Enable back

--- themes.mk
+++ themes.mk
@@ -1,14 +1,14 @@
 # Navbar
-#PRODUCT_PACKAGES += \
-#    NavbarAsusOverlay \
-#    NavbarOnePlusOverlay \
-#    NavbarOneUiOverlay \
-#    NavbarTecnoCamonOverlay \
-#    NavbarAndroidOverlay \
-#    NavbarMotoOverlay \
-#    NavbarNexusOverlay \
-#    NavbarOldOverlay \
-#    NavbarSammyOverlay
+PRODUCT_PACKAGES += \
+    NavbarAsusOverlay \
+    NavbarOnePlusOverlay \
+    NavbarOneUiOverlay \
+    NavbarTecnoCamonOverlay \
+    NavbarAndroidOverlay \
+    NavbarMotoOverlay \
+    NavbarNexusOverlay \
+    NavbarOldOverlay \
+    NavbarSammyOverlay
 
 PRODUCT_PACKAGES += \
 	NavigationBarModeGesturalOverlayFS
