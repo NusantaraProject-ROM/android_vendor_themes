863e99aea24174205e073f7660970d4587545bf3
goodmeow <harunbam3@gmail.com>
2022-03-25 15:28:34 +0530

    themes: update 1080 new bootanimation add add 1440

--- bootanimation.mk
+++ bootanimation.mk
@@ -1,6 +1,8 @@
 # Boot Animation
 
-ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
+PRODUCT_COPY_FILES += vendor/themes/bootanimation/1440.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
+else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 PRODUCT_COPY_FILES += vendor/themes/bootanimation/1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
 PRODUCT_COPY_FILES += vendor/themes/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
