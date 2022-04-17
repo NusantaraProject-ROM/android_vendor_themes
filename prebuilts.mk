64eb7fb8341110435a4601183391ff3f286a282a
Genkzsz11 <genkzsz11@gmail.com>
2022-04-18 02:37:35 +0700

    Revert "themes: include SoundPickerPrebuilt inside guard non-gapps"
    This reverts commit 84d14bb555cc26d1571ca7a74da523c874f7e303.

--- prebuilts.mk
+++ prebuilts.mk
@@ -1,10 +1,11 @@
-ifneq ($(USE_GAPPS), true)
 # Prebuilt app
 PRODUCT_PACKAGES += \
-   SoundPickerPrebuilt \
+   SoundPickerPrebuilt
+
+ifneq ($(USE_GAPPS), true)
+PRODUCT_PACKAGES += \
    GalleryGoPrebuilt \
    ViaPrebuilt
-
 ifeq ($(USE_MICROG),true)
 PRODUCT_PACKAGES += \
    SimpleGalleryPrebuilt
